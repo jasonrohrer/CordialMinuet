62df644bb9ae9456de3da8adef2edc53e3fc93b7
Jason Rohrer <jasonrohrer@fastmail.fm>
2014-11-12 09:44:14 -0800

    Getting ready to build v7.

--- gameSource/game.cpp
+++ gameSource/game.cpp
@@ -1,10 +1,10 @@
-int versionNumber = 6;
+int versionNumber = 7;
 
 // retain an older version number here if server is compatible
 // with older client versions.
 // Change this number (and number on server) if server has changed
 // in a way that breaks old clients.
-int accountHmacVersionNumber = 5;
+int accountHmacVersionNumber = 7;
 
 
 
