3fb2a536b4d4d94eb0cfab8a8cf9ade938d93ab1
Jason Rohrer <jasonrohrer@fastmail.fm>
2014-12-02 09:59:27 -0800

    Getting ready to release v10.

--- gameSource/game.cpp
+++ gameSource/game.cpp
@@ -1,4 +1,4 @@
-int versionNumber = 9;
+int versionNumber = 10;
 
 // retain an older version number here if server is compatible
 // with older client versions.
--- server/server.php
+++ server/server.php
@@ -8,7 +8,7 @@
 
 // server will tell clients to upgrade to this version
 global $cm_version;
-$cm_version = "9";
+$cm_version = "10";
 
 
 // leave an older version here IF older clients can also connect safely
