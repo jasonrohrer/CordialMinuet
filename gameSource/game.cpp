3ad61b88079885f8433440e0c45f3a60eb914059
Jason Rohrer <jasonrohrer@fastmail.fm>
2015-01-08 15:52:06 -0800

    Added missing version change for game.cpp

--- gameSource/game.cpp
+++ gameSource/game.cpp
@@ -1,10 +1,10 @@
-int versionNumber = 11;
+int versionNumber = 12;
 
 // retain an older version number here if server is compatible
 // with older client versions.
 // Change this number (and number on server) if server has changed
 // in a way that breaks old clients.
-int accountHmacVersionNumber = 9;
+int accountHmacVersionNumber = 12;
 
 
 
