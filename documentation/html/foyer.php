aae324b7a43c9eaa83ab97422ff5fe870bf6f1b1
Jason Rohrer <jasonrohrer@fastmail.fm>
2015-02-02 11:33:04 -0800

    Added more welcoming foyer page.

---
+++ documentation/html/foyer.php
@@ -0,0 +1,85 @@
+<?php include( "head.php" ); ?>
+
+<center>
+
+
+<font size=7>CORDIAL MINUET</FONT><br>
+A two-player ONLINE STRATEGY GAME played for REAL MONEY by <a href="http://hcsoftware.sf.net/jason-rohrer">JASON ROHRER</a><br>
+
+<br>
+<br>
+
+DOWNLOAD the game:<br>
+<br>
+[<a href="http://cordialminuet.com/coastcaravan/CordialMinuet_v12_Windows.exe">WINDOWS</a>]
+- -- -
+[<a href="http://cordialminuet.com/coastcaravan/CordialMinuet_v12_IntelMacOSX.dmg">MAC</a>]
+- -- -
+[<a href="http://cordialminuet.com/coastcaravan/CordialMinuet_v12_Linux.tar.gz">LINUX</a>]
+- -- -
+[<a href="http://cordialminuet.com/coastcaravan/CordialMinuet_v12_UnixSource.tar.gz">SOURCE</a>]
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+
+MINIMUM deposit is $2.00<br>
+After card processing FEES you will have $1.64<br>
+This is enough for ONE HUNDRED AND SIXTY FOUR penny games<br>
+Even if you lose EVERY GAME<br>
+But you will not lose, you will WIN some<br>
+Practice for CHEAP but never for FREE<br>
+Free is not REAL MONEY
+
+<br>
+<br>
+<br>
+<br>
+
+
+
+
+
+HOW to play:<br>
+<br>
+
+<iframe title="YouTube video player" width="560" height="315" src="http://www.youtube.com/embed/wZcmUGMj30k?rel=0" frameborder="0" allowfullscreen></iframe>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+DISCUSS the game:<br>
+<br>
+
+<a href="http://cordialminuet.com/incrementensemble/forums">CORDIAL MINUET forums</a>
+
+
+<br>
+<br>
+<br>
+<br>
+
+And most importantly:<br>
+<br>
+WELCOME
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+
+</center>
+
+<?php include( "foot.php" ); ?>
