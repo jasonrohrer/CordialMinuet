a0fa9da8b076a239f7618bf2803ae7a682a875b8
Jason Rohrer <jasonrohrer@fastmail.fm>
2026-05-08 19:45:54 -0400

    Made cm-specific changes to backup scripts

--- server/scripts/makeBackups.sh
+++ server/scripts/makeBackups.sh
@@ -3,25 +3,20 @@ pw="SECRET"
 
 date=`date +"%Y_%b_%d_%a"`
 
-mysqldump -u jcr13_dbU --password=$pw jcr13_cdTicket > /home/jcr13/backups/cd_ticket_$date.mysql
-gzip -f /home/jcr13/backups/cd_ticket_$date.mysql
 
-mysqldump -u jcr13_dbU --password=$pw jcr13_cdGame > /home/jcr13/backups/cd_game_$date.mysql
-gzip -f /home/jcr13/backups/cd_game_$date.mysql
+mysqldump -u jcr13_dbU --password=$pw jcr13_cmGame > /home/jcr13/backups/cm_game_$date.mysql
+gzip -f /home/jcr13/backups/cm_game_$date.mysql
 
 
-mysqldump -u jcr13_dbU --password=$pw jcr13_cdForums > /home/jcr13/backups/cd_forums_$date.mysql
-gzip -f /home/jcr13/backups/cd_forums_$date.mysql
-
-mysqldump -u jcr13_dbU --password=$pw jcr13_cdNews > /home/jcr13/backups/cd_news_$date.mysql
-gzip -f /home/jcr13/backups/cd_news_$date.mysql
+mysqldump -u jcr13_dbU --password=$pw jcr13_cdForums > /home/jcr13/backups/cm_forums_$date.mysql
+gzip -f /home/jcr13/backups/cm_forums_$date.mysql
 
 
 
 # now all website php stuff, which contains settings files
 # that have been modified from git versions
 
-dirName=cd_webPHP_$date
+dirName=cm_webPHP_$date
 dirPath=/home/jcr13/backups/$dirName
 
 rm -rf $dirPath
@@ -41,13 +36,13 @@ rm -r $dirName
 
 # now binary builds, since they are a pain to re-make if lost
 
-dirName=cd_builds_$date
+dirName=cm_builds_$date
 dirPath=/home/jcr13/backups/$dirName
 
 rm -rf $dirPath
 mkdir $dirPath
 
-cp /home/jcr13/cdDownloads/* $dirPath
+cp /home/jcr13/public_html/coastcaravan/* $dirPath
 
 cd /home/jcr13/backups
 
